9b7962adda3023de75caab6427182f32e67f29f8
tvasubabu <tvasubabu@gmail.com>
2019-04-01 19:10:19 +0530

    Java ArrayList Basics

---
+++ Java/arrayList.java
@@ -0,0 +1,54 @@
+
+// All the collections are defined in java.util package
+import java.util.*;
+
+public class arrayList {
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> alist = new ArrayList<Integer>();
+		
+		alist.add(20);
+		alist.add(30);
+		alist.add(40);
+		alist.add(50);
+		alist.add(0,10); // Add element at specified index
+		System.out.printf("Size of the arrayList is %d\n", + alist.size());
+		
+		// Processing all the elements in ArrayList
+		for ( Integer i: alist) {
+			System.out.println(i);
+		}
+		
+		System.out.println(alist);
+		System.out.println("\n");
+		
+		//Iterator
+		Iterator<Integer> iter = alist.iterator();
+		while(iter.hasNext()) {			
+			System.out.println(iter.next());			
+		}		
+		System.out.println("\n");
+		
+		//ListIterator
+		ListIterator<Integer> new_iter = alist.listIterator();
+		while(new_iter.hasNext()) {			
+			System.out.println(new_iter.next());			
+		}
+		//remove
+		alist.remove(0);
+		
+		//find and remove an element
+		alist.remove(alist.indexOf(20));
+		
+		alist.add(0,10);
+		
+		//update
+		alist.set(0, 20);
+		
+		System.out.println("\n");
+		for ( Integer i: alist) {
+			System.out.println(i);
+		}
+	}
+
+}
