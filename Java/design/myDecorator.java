7359bd5fe4a401f4d85c28b00ad93e478a44ac2c
tvasubabu <tvasubabu@gmail.com>
2019-04-05 21:58:02 +0530

    decorator pattern

--- Java/design/myDecorator.java
+++ Java/design/myDecorator.java
@@ -1,5 +1,100 @@
+/*
+ * date: 05-Apr-2019
+ * @author - T Vasu Babu
+ * 
+ * Example of Decorator design pattern
+ * 
+ * References: Head First Design Patterns
+ */
+
 package design;
 
-public class myDecorator {
+abstract class Beverage {
+	String description = "Unknown Beverage";
+	
+	public String getDescription()
+	{
+		return description;		
+	}
+	abstract public int cost();	
+}
 
+class Tea extends Beverage {
+	Tea()
+	{
+		description = "Tea";
+	}
+	public int cost()
+	{
+		return 10;
+	}
+}
+
+class Coffee extends Beverage {
+	Coffee()
+	{
+		description = "Coffee";
+	}
+	public int cost()
+	{
+		return 20;
+	}
+}
+
+abstract class CondimentDecorator extends Beverage {
+	abstract public int cost();	
+}
+
+class Chocolate extends CondimentDecorator {
+	Beverage b1;
+	
+	Chocolate(Beverage b1)
+	{
+		this.b1 = b1;
+	}
+	
+	public String getDescription()
+	{
+		return ("Chocolate" + " + " + b1.getDescription());
+	}
+	
+	public int cost()
+	{
+		return (5 + b1.cost());
+	}
+}
+
+class Sugar extends CondimentDecorator {
+	Beverage b1;
+	
+	Sugar(Beverage b1)
+	{
+		this.b1 = b1;
+	}
+	
+	public String getDescription()
+	{
+		return ("Sugar" + " + " + b1.getDescription());
+	}
+	
+	public int cost()
+	{
+		return (2 + b1.cost());
+	}		
+}
+
+public class myDecorator {
+	public static void main(String[] args) {
+		Beverage b1 = new Tea();
+		
+		System.out.println(b1.getDescription() + " " + b1.cost());
+		
+		b1 = new Chocolate(b1);
+		
+		System.out.println(b1.getDescription() + " " + b1.cost());
+		
+		b1 = new Sugar(b1);
+		
+		System.out.println(b1.getDescription() + " " + b1.cost());
+	}	
 }
